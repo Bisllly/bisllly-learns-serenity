c787e54e5496104644afd6af4f4824c46d9f89eb
Bisllly <111986298+Bisllly@users.noreply.github.com>
2023-06-15 13:45:05 +0700

    Hide URL

--- src/test/java/no/restaff/fresher/ui/SignInPage.java
+++ src/test/java/no/restaff/fresher/ui/SignInPage.java
@@ -3,5 +3,5 @@ package no.restaff.fresher.ui;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://preview.oceansmart.com/signin")
+@DefaultUrl("***********************")
 public class SignInPage extends PageObject {}
